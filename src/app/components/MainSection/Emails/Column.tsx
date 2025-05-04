033ae7d9114728b4014982ab8362321c48240422
Imran Shah <imranshah10140@gmail.com>
2025-05-04 15:35:35 +0530

    Fix

--- src/app/components/MainSection/Emails/Column.tsx
+++ src/app/components/MainSection/Emails/Column.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   SortableContext,
@@ -6,18 +7,27 @@ import {
 import React from "react";
 import Email from "./Email";
 
-const Column = ({ tasks }) => {
+const Column = ({ tasks }: any) => {
   return (
     <div className="flex flex-col overflow-x-scroll h-screen">
       <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
-        {tasks.map((data, i) => (
-          <Email
-            id={data.id}
-            title={data.title}
-            subject={data.subject}
-            key={data.id}
-          />
-        ))}
+        {tasks.map(
+          (
+            data: {
+              id: any | null | undefined;
+              title: string;
+              subject: string;
+            },
+            i: any
+          ) => (
+            <Email
+              id={data.id}
+              title={data.title}
+              subject={data.subject}
+              key={data.id}
+            />
+          )
+        )}
       </SortableContext>
     </div>
   );
